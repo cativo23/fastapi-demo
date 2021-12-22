711bc6e3770ac2d5b70a4a33ca4b00147c36e40d
Carlos Cativo <cativo23.kt@gmail.com>
2021-12-21 18:45:56 -0600

    🔨Add blog POST method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,3 +20,57 @@ def about():
             "age": 25
         }
     }
+
+@app.get('/blog')
+def blog(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+
+    if published:
+      #Only get 10 published blogs
+        return {
+            "data": f'List of {limit} published blog(s)'
+        }  
+    else:
+        #Only get 10 published blogs
+        return {
+            "data": f'List of {limit} unpublished blog(s)'
+        }
+    
+
+@app.get('/blog/unpublished')
+def unpublished():
+    #TODO: Fetch blog with id = id
+    return {
+        "data": "unplublished list"
+    }
+
+@app.get('/blog/{id}')
+def show(id: int):
+    #TODO: Fetch blog with id = id
+    return {
+        "data": id
+    }
+
+@app.get('/blog/{id}/comment')
+def comments(id: int):
+    #TODO: Fetch messages for blog with id = id
+    return {
+        "data": {
+            "comments": [
+                {
+                    "id": 12,
+                    "text": "Text"
+                }
+            ],
+        }
+    }
+
+class Blog(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'blog created with title: {blog.title}'}
